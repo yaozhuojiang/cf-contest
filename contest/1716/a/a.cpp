ae6fd8c18f95b9f8e97337eee724da3ff1ba839e
yaozhuojiang <yaozhuojiang@gmail.com>
2022-08-15 22:19:43 +0800

    feat: start the codeforces project

---
+++ contest/1716/a/a.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fi first
+#define se second
+#define db double
+#define ll long long
+#define pb push_back
+#define lchild (k<<1)
+#define rchild (k<<1|1)
+#define pii pair<int,int>
+#define mm(a,b) memset(a,b,sizeof(a))
+#define rush() int T; cin >> T; while (T--)
+#define rep(i,a,b) for(int i=(a);i<=(b);i++)
+#define per(i,a,b) for(int i=(a);i>=(b);i--)
+#define ACCELERATE (ios::sync_with_stdio(false),cin.tie(0))
+const int N = 2e5 + 5;
+const int M = (N << 2);
+const int P = 1e9 + 7;
+const int inf = 0x3f3f3f3f;
+const ll INF = 0x3f3f3f3f3f3f3f3f;
+int n;
+int main()
+{
+	ACCELERATE;
+	rush() {
+		cin >> n;
+		if (n == 1) cout << 2 << endl;
+		else if (n % 3 == 2) cout << (n / 3 + 1) << endl;
+		else if (n % 3 == 1) cout << (n - 4) / 3 + 2 << endl;
+		else cout << n / 3 << endl;	
+	}
+	return 0;
+}

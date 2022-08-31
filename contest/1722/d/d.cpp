d9c5d4244322bb58b4ceefe4af05b7093356db59
yaozhuojiang <yaozhuojiang@gmail.com>
2022-08-31 11:36:01 +0800

    feat: solve 1722/d

--- contest/1722/d/d.cpp
+++ contest/1722/d/d.cpp
@@ -18,10 +18,28 @@ const int M = (N << 2);
 const int P = 1e9 + 7;
 const int inf = 0x3f3f3f3f;
 const ll INF = 0x3f3f3f3f3f3f3f3f;
-
+int n;
+string s;
 int main()
 {
 	ACCELERATE;
-	
+	rush() {
+		cin >> n >> s;
+		s = '0' + s;
+		vector<int> v;
+		ll ans = 0;
+		rep(i, 1, n) {
+			int left = i - 1, right = n - i;
+			v.push_back(s[i] == 'L' ? max(0, right - left) : max(left - right, 0));
+			ans += (s[i] == 'L' ? left : right);
+		}
+		sort(v.begin(), v.end());
+		reverse(v.begin(), v.end());
+		for(auto &x : v) {
+			ans += x;
+			cout << ans << " ";
+		}
+		cout << endl;
+	}
 	return 0;
 }

e5e940a64ba2d0eba0ea64570d092c66fb3928e7
yaozhuojiang <yaozhuojiang@gmail.com>
2022-09-02 10:37:59 +0800

    feat: solve 1693/b

--- contest/1693/b/b.cpp
+++ contest/1693/b/b.cpp
@@ -18,10 +18,35 @@ const int M = (N << 2);
 const int P = 1e9 + 7;
 const int inf = 0x3f3f3f3f;
 const ll INF = 0x3f3f3f3f3f3f3f3f;
-
+int n, p[N], ans;
+ll l[N], r[N], m[N];
+vector<vector<int> > e;
+void dfs(int u, int f)
+{
+	ll child = 0;
+	for (auto &v : e[u]) {
+		dfs(v, u);
+		child += m[v];
+	}
+	if (child < l[u]) {
+		ans++;
+		m[u] = r[u];
+	} else m[u] = min(child, r[u]);
+}
 int main()
 {
 	ACCELERATE;
-	
+	rush() {
+		cin >> n;
+		e = vector<vector<int> > (n + 1);
+		rep(i, 2, n) {
+			cin >> p[i];
+			e[p[i]].push_back(i);
+		}
+		rep(i, 1, n) cin >> l[i] >> r[i];
+		ans = 0;
+		dfs(1, -1);
+		cout << ans << endl;
+	}
 	return 0;
 }

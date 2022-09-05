980d4cdbcf4cea86571df798182bfbed035d8ef4
yaozhuojiang <yaozhuojiang@gmail.com>
2022-09-05 15:30:03 +0800

    feat: solve 1717/d

--- contest/1717/d/d.cpp
+++ contest/1717/d/d.cpp
@@ -18,10 +18,31 @@ const int M = (N << 2);
 const int P = 1e9 + 7;
 const int inf = 0x3f3f3f3f;
 const ll INF = 0x3f3f3f3f3f3f3f3f;
-
+int n, k;
+ll qpow(ll a, ll b)
+{
+	ll ans = 1ll; a %= P;
+	for (ll i = b; i; i >>= 1, a = a * a % P) if (i & 1) ans = ans * a % P;
+	return ans % P;
+}
+ll fac[N], inv[N];
+void getfac()
+{
+	fac[0] = inv[0] = 1;
+	rep(i, 1, N - 1) fac[i] = (fac[i - 1] * i) % P, inv[i] = (inv[i - 1] * qpow(i, P - 2)) % P;
+}
+ll getC(ll n, ll m) { return (fac[n] * inv[n - m] % P) * inv[m] % P; }
 int main()
 {
 	ACCELERATE;
-	
+	getfac();
+	cin >> n >> k;
+	if (k >= n) cout << qpow(2, n) << endl;
+	else {
+		ll ans = 0;
+		// rep(i, 0, k) cout << getC(3, i) << endl;
+		rep(i, 0, k) ans = (ans + getC(n, i)) % P;
+		cout << ans << endl;
+	}
 	return 0;
 }

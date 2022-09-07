fda75d7b68b2c0ce766242ae2379c0cfa0e30e45
yaozhuojiang <yaozhuojiang@gmail.com>
2022-09-07 16:19:48 +0800

    feat: solve 1726/a

--- contest/1726/a/a.cpp
+++ contest/1726/a/a.cpp
@@ -18,40 +18,18 @@ const int M = (N << 2);
 const int P = 1e9 + 7;
 const int inf = 0x3f3f3f3f;
 const ll INF = 0x3f3f3f3f3f3f3f3f;
-
-int a[2005];
-
+int n, a[N];
 int main()
 {
 	ACCELERATE;
-	
-	rush(){
-		int n;
+	rush() {
 		cin >> n;
-		
-		for (int i = 0; i < n; i++){
-			cin>>a[i];
-		}
-		if(n==1) {
-			cout<<0<<endl;
-			continue;
-		}
-		int ma = -1, maxcha = -1e9;  // a[]最大值、前一个减后一个的差最大值
-		for (int i = 0; i < n-1; i++){
-			int cha = a[i] - a[i+1];
-			maxcha = max(maxcha, cha);
-
-			ma = max(ma, a[i]);
-		}
-		ma = max(ma, a[n-1]);
-		maxcha = max(maxcha, a[n-1]-a[0]);
-
-		cout<<ma<<' '<<maxcha<<' '<<a[0]<<endl;
-		
-
-		cout << max(ma-a[0], maxcha) << endl;
-
+		rep(i, 0, n - 1) cin >> a[i];
+		int ans = 0;
+		rep(i, 0, n - 1) ans = max(ans, (a[(i + n - 1) % n] - a[i]));
+		rep(i, 1, n - 1) ans = max(ans, a[i] - a[0]);
+		rep(i, 0, n - 2) ans = max(ans, a[n - 1] - a[i]);
+		cout << ans << endl;
 	}
-
 	return 0;
 }

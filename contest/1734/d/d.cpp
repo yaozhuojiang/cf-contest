76666c635e53c60f6ec764062d5a265c56f0c34b
yaozhuojiang <yaozhuojiang@gmail.com>
2022-09-30 10:14:05 +0800

    feat: solve 1734/d

--- contest/1734/d/d.cpp
+++ contest/1734/d/d.cpp
@@ -3,8 +3,8 @@ using namespace std;
 #define fi first
 #define se second
 #define db double
+#define int long long 
 #define ll long long
-#define int long long
 #define pb push_back
 #define lchild (k<<1)
 #define rchild (k<<1|1)
@@ -20,43 +20,56 @@ const int P = 1e9 + 7;
 const int inf = 0x3f3f3f3f;
 const ll INF = 0x3f3f3f3f3f3f3f3f;
 int n, k, a[N], pre[N];
-int main()
+int32_t main()
 {
 	ACCELERATE;
 	rush() {
 		cin >> n >> k;
 		rep(i, 1, n) cin >> a[i];
 		rep(i, 1, n) pre[i] = pre[i - 1] + a[i];
-		int l = k - 1;
+		int l = k - 1, r = k + 1;
 		vector<pii> L, R;
-		rep(i, k - 1, 1) {
-			if (pre[i] <= pre[l] || i == 0) {
-				int cur = 0, mn = -INF;
-				rep(j, l, i) {
-					cur += a[j];
-					mn = min(mn, cur);
-				}
+		int cost = 0, bad = INF;
+		per(i, k - 1, 1) {
+			cost += a[i];
+			bad = min(bad, cost);
+			if (pre[i - 1] <= pre[l] || i == 1) {
+				L.push_back({cost, -bad});
+				cost = 0;
+				bad = INF;
 				l = i - 1;
-				L.push_back({cur, mn});
 			}
 		}
-		int r = k + 1;
 		rep(i, k + 1, n) {
-			if (pre[i] >= pre[r] || i == n) {
-				int cur = 0, mn = -INF;
-				rep(j, r, i) {
-					cur += a[j];
-					mn = min(mn, cur);
-				}
+			cost += a[i];
+			bad = min(bad, cost);
+			if (pre[i] >= pre[r - 1] || i == n) {
+				R.push_back({cost, -bad});
+				cost = 0;
+				bad = INF;
 				r = i + 1;
-				R.push_back({cur, mn});
 			}
 		}
+		int cur = a[k];
 		reverse(L.begin(), L.end());
 		reverse(R.begin(), R.end());
 		while (233) {
-			if (L.
-
+			bool flag = false;
+			if (!L.empty() && cur >= L.back().second) {
+				cur += L.back().first;
+				L.pop_back();
+				flag = true;
+			}
+			if (!R.empty() && cur >= R.back().second) {
+				cur += R.back().first;
+				R.pop_back();
+				flag = true;
+			}
+			if (!flag) break;
+			if (L.empty() || R.empty()) break;
+		}
+		if (L.empty() || R.empty()) cout << "YES" << endl;
+		else cout << "NO" << endl;
 	}
 	return 0;
 }

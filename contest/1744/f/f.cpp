62c9372bfbed272bc39b43d5d896a073c80f1917
yaozhuojiang <yaozhuojiang@gmail.com>
2022-10-17 16:28:13 +0800

    feat: solve 1744/f

--- contest/1744/f/f.cpp
+++ contest/1744/f/f.cpp
@@ -18,10 +18,36 @@ const int M = (N << 2);
 const int P = 1e9 + 7;
 const int inf = 0x3f3f3f3f;
 const ll INF = 0x3f3f3f3f3f3f3f3f;
-
+int n, p[N], idx[N];
 int main()
 {
 	ACCELERATE;
-	
+	rush() {
+		cin >> n;
+		rep(i, 1, n) cin >> p[i];
+		rep(i, 1, n) idx[p[i]] = i;
+		idx[n] = 0;
+		int left = idx[0], right = left;
+		ll ans = 0;
+		rep(i, 1, n) {
+			int next = idx[i], len = i * 2;
+			if (next < left) {
+				per(j, left, next + 1) {
+					int lim = min(n, j + len - 1);
+					if (lim < right) break;
+					else ans += lim - right + 1ll;
+				}
+				left = next;
+			} else if (next > right) {
+				rep(j, right, next - 1) {
+					int lim = max(1, j - len + 1);
+					if (lim > left) break;
+					else ans += left - lim + 1ll;
+				}
+				right = next;
+			}
+		}
+		cout << ans << endl;
+	}
 	return 0;
 }
